9f22959eecc4d04c63c969b90456a5cc5180f3aa
tylerchen14 <tylerchenpo@gmail.com>
2024-01-31 14:00:45 +0800

    嘗試加入 search bar

--- src/chatroom/parts-get-point/packageUp.php
+++ src/chatroom/parts-get-point/packageUp.php
@@ -7,7 +7,7 @@
         </button>
       </div>
       <div>
-        <a class="navbar-brand brand-logo" href="../index.php">
+        <a class="navbar-brand brand-logo" href="../index_.php">
           <img src="../assets/images/ruined.png" alt="logo" />
         </a>
       </div>
@@ -160,33 +160,6 @@
       </button>
     </div>
   </nav>
-  <!-- partial -->
-  <div class="container-fluid page-body-wrapper">
-    <!-- partial:partials/_settings-panel.html -->
-    <!-- <div class="theme-setting-wrapper">
-  <div id="settings-trigger"><i class="ti-settings"></i></div>
-  <div id="theme-settings" class="settings-panel">
-    <i class="settings-close ti-close"></i>
-    <p class="settings-heading">SIDEBAR SKINS</p>
-    <div class="sidebar-bg-options selected" id="sidebar-light-theme">
-      <div class="img-ss rounded-circle bg-light border me-3"></div>Light
-    </div>
-    <div class="sidebar-bg-options" id="sidebar-dark-theme">
-      <div class="img-ss rounded-circle bg-dark border me-3"></div>Dark
-    </div>
-    <p class="settings-heading mt-2">HEADER SKINS</p>
-    <div class="color-tiles mx-0 px-4">
-      <div class="tiles success"></div>
-      <div class="tiles warning"></div>
-      <div class="tiles danger"></div>
-      <div class="tiles info"></div>
-      <div class="tiles dark"></div>
-      <div class="tiles default"></div>
-    </div>
-  </div>
-</div> -->
-
-    <!-- partial -->
     <!-- partial:partials/_sidebar.html -->
     <nav class="sidebar sidebar-offcanvas" id="sidebar">
       <ul class="nav">
