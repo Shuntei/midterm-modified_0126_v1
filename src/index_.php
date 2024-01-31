b43ba35fae24cadc9bfd8539fefe425eda11c330
tylerchen14 <tylerchenpo@gmail.com>
2024-01-31 15:44:51 +0800

    改 index

--- src/index_.php
+++ src/index_.php
@@ -486,7 +486,6 @@ $title = isset($title) ? $title . '-MFEE47_02' : 'MFEE47_02'
               <ul class="nav flex-column sub-menu">
                 <li class="nav-item"> <a class="nav-link" href="./shoppingCart/ca_merchandise_list_admin.php">商品列表</a></li>
                 <li class="nav-item"> <a class="nav-link" href="./shoppingCart/ca_cart_list_admin.php">購物車</a></li>
-
               </ul>
             </div>
           </li>
