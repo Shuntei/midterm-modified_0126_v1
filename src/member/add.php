e68f1ac5344c005ef6cca450e2b004ffddf793c7
linda <nmandakh60@gmail.com>
2024-02-01 16:41:40 +0800

    add information validation & check existing info

--- src/member/add.php
+++ src/member/add.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require "./parts/db_connect_midterm.php";
 include "./parts/html-head.php";
 include "./../package/packageUp.php";
@@ -20,55 +20,55 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
                     <p class="card-description text-center">
                         Please fill the information correctly
                     </p>
-                    <form class="forms-sample" name="form1" method="post" onsubmit="sendForm(event)">
-                        <input type="text" class="form-control" id="userId" hidden name="userId" value="<?= $row['user_id'] ?>">
+                    <form class="forms-sample" name="form1" method="post" onsubmit="sendForm(event)" novalidate>
                         <div class="form-group row align-items-start">
                             <label for="name" class="col-sm-3 col-form-label">Name</label>
                             <div class="col-sm-9 mt-2">
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Username">
+                                <input type="text" class="form-control" id="name" name="name" required placeholder="Username">
                                 <p class="text-danger mt-1"></p>
                             </div>
                         </div>
                         <div class="form-group row align-items-start">
                             <label for="email" class="col-sm-3 col-form-label">Email</label>
                             <div class="col-sm-9 mt-2">
-                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                                <div class="form-text text-danger"></div>
+                                <input type="email" class="form-control" id="email" name="email" required placeholder="Email">
+                                <p class="text-danger mt-1"></p>
                             </div>
                         </div>
                         <div class="form-group row align-items-start">
                             <label for="phone" class="col-sm-3 col-form-label">Mobile</label>
                             <div class="col-sm-9 mt-2">
-                                <input type="text" class="form-control" id="phone" name="phone" placeholder="Mobile number">
-                                <div class="form-text text-danger"></div>
+                                <input type="text" class="form-control" id="phone" name="phone" required placeholder="Mobile number">
+                                <p class="text-danger mt-1"></p>
                             </div>
                         </div>
                         <div class="form-group row align-items-start mb-0">
                             <label for="password" class="col-sm-3 col-form-label">Password</label>
                             <div class="col-sm-9 mt-2">
-                                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                                <p class="card-description mt-1">The password needs to include 6-16 characters, at least one number and one special character</div>
+                                <input type="password" class="form-control" id="password" name="password" required placeholder="Password">
+                                <p class="card-description mt-1">The password needs to include 6-16 characters, at least one number and one special character
+                            </div>
                             </p>
                         </div>
                         <div class="form-group row align-items-start mt-0">
                             <label for="rePassword" class="col-sm-3 col-form-label">Re Password</label>
                             <div class="col-sm-9 mt-2">
-                                <input type="password" class="form-control" id="rePassword" name="rePassword" placeholder="Password">
-                                <div class="form-text text-danger"></div>
+                                <input type="password" class="form-control" id="rePassword" name="rePassword" required placeholder="Password">
+                                <p class="text-danger mt-1"></p>
                             </div>
                         </div>
                         <div class="form-group row align-items-start">
                             <label for="picture" class="col-sm-3 col-form-label">Profile Picture</label>
                             <div class="col-sm-9 mt-2 ">
                                 <input type="file" class="form-control" id="picture" name="picture">
-                                <div class="form-text text-danger"></div>
+                                <p class="text-danger mt-1"></p>
                             </div>
                         </div>
                         <div class="form-group row align-items-start">
                             <label for="birthday" class="col-sm-3 col-form-label">Birthday</label>
                             <div class="col-sm-9 mt-2">
-                                <input type="date" class="form-control" id="birthday" name="birthday" value="<?= $row['birthday'] ?>">
-                                <div class="form-text text-danger"></div>
+                                <input type="date" class="form-control" id="birthday" name="birthday" required>
+                                <p class="text-danger mt-1"></p>
                             </div>
                         </div>
                         <div class="form-group row align-items-start">
@@ -85,6 +85,7 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
                                 <!-- dropdown end -->
                             </div>
                         </div>
+                        <p class="text-danger mt-1" id="requiredInfo"></p>
                         <div class="d-flex justify-content-end">
                             <button type="submit" class="btn btn-primary me-2">Add</button>
                             <button type="button" class="btn btn-light">Clear</button>
@@ -123,7 +124,8 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
     }
 
     function validatePass(password) {
-        const passwordRe = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        // const passwordRe = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        const passwordRe = /^(?=.*\d)(?=.*[!@#$%^&*()-_+={}[\]|;:"<>,./?])[\w!@#$%^&*()-_+={}[\]|;:"<>,./?]{6,16}$/;
         return passwordRe.test(password);
     }
 
@@ -135,20 +137,31 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
         const passwordError = password.nextElementSibling
         let errorMessage = 'The password needs to include ';
 
-        if(password.length < 6 || password.length > 16){
+        if (password.value.length < 6 || password.value.length > 16) {
             errorMessage += '6-16 characters, ';
+        } else {
+            errorMessage = '';
         }
 
-        if(!/\d/.test(password)){
+        if (!/\d/.test(password.value)) {
             errorMessage += 'at least one number, ';
         }
 
-        if(!/[!@#$%^&*]/.test(password)){
+        if (!/[!@#$%^&*]/.test(password.value)) {
             errorMessage += 'at least one special character, '
         }
 
         errorMessage = errorMessage.replace(new RegExp(', $'), '');
-        passwordError.innerHTML = errorMessage
+
+        if (!(errorMessage == 'The password needs to include ')) {
+            passwordError.innerHTML = errorMessage
+        } else {
+            password.innerHTML = '';
+        }
+
+        if (errorMessage) {
+            passwordError.classList.add('text-danger')
+        }
     }
 
     function validateDate(birthday) {
@@ -156,27 +169,47 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
         return dateRe.test(birthday);
     }
 
-    function validatePassword(){
+    function validatePassword() {
         const passwordError = password.nextElementSibling;
 
-        if(!validatePass(password)){
+        if (!validatePass(password)) {
             updatePassErrorMessage(password);
         } else {
             passwordError.innerHTML = '';
         }
     }
 
-    function validateRePassword(){
+    function validateRePassword() {
         const rePasswordError = rePassword.nextElementSibling;
-        if(!validateRePass(password.value, rePassword.value)){
+        if (!validateRePass(password.value, rePassword.value)) {
             rePasswordError.innerHTML = 'Passwords do not match'
         } else {
             rePasswordError.innerHTML = ''
         }
-    } 
+    }
 
-    password.addEventListener('input', validatePassword)
-    rePassword.addEventListener('input', validateRePassword)
+    password.addEventListener('input', () => {
+        validatePassword()
+    })
+
+    rePassword.addEventListener('input', () => {
+        validateRePassword();
+    })
+
+    const requiredFields = document.querySelectorAll('[required]')
+
+
+
+    requiredFields.forEach(field => {
+        field.addEventListener('input', () => {
+            const fieldValue = field.value.trim()
+            if (fieldValue) {
+                name.nextElementSibling.innerHTML = ''
+                requiredInfo.innerHTML = ''
+                field.style.border = '1px solid #dee2e6'
+            }
+        })
+    })
 
     const sendForm = e => {
         e.preventDefault();
@@ -191,10 +224,33 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
 
         let isPass = true;
 
+        const requiredFields = document.querySelectorAll('[required]')
+        const requiredInfo = document.querySelector('#requiredInfo')
+
+        requiredFields.forEach(field => {
+            const fieldName = field.getAttribute('name')
+            const fieldValue = field.value.trim();
+
+            if (!fieldValue) {
+                isPass = false;
+                // field.nextElementSibling.innerHTML = `Please enter ${fieldName}`
+                requiredInfo.innerHTML = 'Please fill out the required information'
+                field.style.border = '1px solid #f95f53'
+                field.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'
+                })
+            }
+        })
+
         if (!validateName(name.value)) {
             isPass = false;
             name.nextElementSibling.innerHTML = "Please enter a correct name"
             name.style.border = '1px solid #f95f53';
+            name.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+            })
         }
 
         if (email.value && !validateEmail(email.value)) {
@@ -239,6 +295,24 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
                                     swal("New user has been added", {
                                         icon: "success",
                                     });
+                                } else {
+                                    switch (result.code) {
+                                        case 1:
+                                            swal("This email has been registered already", {
+                                                icon: "error",
+                                            });
+                                            break;
+                                        case 2:
+                                            swal("This phone number has been registered already", {
+                                                icon: "error",
+                                            });
+                                            break;
+                                        case 3:
+                                            swal("Unsuccessful", {
+                                                icon: "error",
+                                            });
+                                            break;
+                                    }
                                 }
                             })
                     }
@@ -250,4 +324,3 @@ $skinRow = $pdo->query($skinSql)->fetchAll();
 
 <?php include "./../package/packageDown.php";
 include "./parts/html-foot.php" ?>
-
